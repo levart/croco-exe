53ede4a84de0d4f52ccca79c30ae0845f2553984
levart <jmukhadze@gmail.com>
2022-08-27 23:49:03 +0400

    main layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {MainLayoutComponent} from "./shared/modules/layout/main-layout/main-layout.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [{
+      path: '',
+      loadChildren: () => import('./features/main/main.module').then(m => m.MainModule)
+    }]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: 'cr-root',
+  template: `<router-outlet></router-outlet>`
 })
 export class AppComponent {
   title = 'croco-test';
