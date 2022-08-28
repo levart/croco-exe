65f5cee90975128e34a18278ec54ce2e8939a8bf
levart <jmukhadze@gmail.com>
2022-08-28 17:13:11 +0400

    default slot redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { MainLayoutComponent } from './features/layout/main-layout/main-layout.component';
-import { SportRoutingModule } from './pages/sport/sport-routing.module';
 
 const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/slots',
+    },
     {
         path: '',
         component: MainLayoutComponent,
